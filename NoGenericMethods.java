794bfd7125c6292827e9417c50cf2b39fca0880d
bowserchris <133642309+bowserchris@users.noreply.github.com>
2023-06-26 09:07:37 +0200

    Update NoGenericMethods.java
    changed type T to String to make it nonGeneric

--- NoGenericMethods.java
+++ NoGenericMethods.java
@@ -1,38 +1,38 @@
 package n1e1;
 
-public class NoGenericMethods <T> {
+public class NoGenericMethods <String> {
 	
-	private T obj1;
-	private T obj2;
-	private T obj3;
+	private String obj1;
+	private String obj2;
+	private String obj3;
 	
-	public NoGenericMethods(T t1, T t2, T t3) {
+	public NoGenericMethods(String t1, String t2, String t3) {
 		this.obj1 = t1;
 		this.obj2 = t2;
 		this.obj3 = t3;
 	}
 
-	public T getObj1() {
+	public String getObj1() {
 		return obj1;
 	}
 
-	public void setObj1(T obj1) {
+	public void setObj1(String obj1) {
 		this.obj1 = obj1;
 	}
 
-	public T getObj2() {
+	public String getObj2() {
 		return obj2;
 	}
 
-	public void setObj2(T obj2) {
+	public void setObj2(String obj2) {
 		this.obj2 = obj2;
 	}
 
-	public T getObj3() {
+	public String getObj3() {
 		return obj3;
 	}
 
-	public void setObj3(T obj3) {
+	public void setObj3(String obj3) {
 		this.obj3 = obj3;
 	}
 
